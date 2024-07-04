1f3546900fc670b0a50ccd1889f1a802e37c0f22
Tania Branchesi <tania.branchesi@grupposcai.it>
2024-07-04 16:54:48 +0200

    creazione component storico-ordini

--- src/app/components/storico-ordini/storico-ordini.component.ts
+++ src/app/components/storico-ordini/storico-ordini.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Prodotto } from 'src/app/model/prodotto';
+import { AcquistiService } from 'src/app/services/acquisti/acquisti.service';
+import { CarrelloService } from 'src/app/services/carrello/carrello.service';
+import { OrdineService } from 'src/app/services/ordine/ordine.service';
 
 @Component({
   selector: 'app-storico-ordini',
@@ -6,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./storico-ordini.component.css']
 })
 export class StoricoOrdiniComponent {
+
+
+  constructor(private ordineService: OrdineService, private acquistiService: AcquistiService){}
+
 
 }
